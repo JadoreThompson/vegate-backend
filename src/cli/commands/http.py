2e8becfe48337dff6f4a361fb5f396dcbe818a9c
LucioJamieAI <148231849+JadoreThompson@users.noreply.github.com>
2026-05-14 16:22:16 +0100

    Refactord http run CLI to call uvicorn.run

--- src/cli/commands/http.py
+++ src/cli/commands/http.py
@@ -3,7 +3,9 @@ from multiprocessing import Process
 import sys
 import time
 import click
+import uvicorn
 
+from config import IS_PRODUCTION
 from runners.api_runner import APIRunner
 from runners.runner_config import RunnerConfig
 from runners.utils import run_runner
@@ -18,40 +20,6 @@ def http():
 
 @http.command(name="run")
 def run():
-    runner_config = RunnerConfig(
-        cls=APIRunner,
-        args=(
-            {
-                "host": "0.0.0.0",
-                "port": 8000,
-                "reload": False,
-            },
-        ),
+    uvicorn.run(
+        "api.app:app", host="0.0.0.0" if IS_PRODUCTION else "localhost", port=8000
     )
-
-    p = Process(target=run_runner, args=(runner_config,), name=runner_config.name)
-    p.start()
-    exit_code = 1
-
-    try:
-        while True:
-            if not p.is_alive():
-                raise RuntimeError(
-                    f"Process '{p.name}' has died (exit code: {p.exitcode})."
-                )
-            time.sleep(0.5)
-    except KeyboardInterrupt:
-        logger.info("Keyboard interrupt — shutting down all processes.")
-        click.echo("\nServer stopped by user")
-        exit_code = 0
-    except Exception as e:
-        logger.exception("Error running server")
-        click.echo(f"Error: {e}", err=True)
-        exit_code = 1
-    finally:
-        if p.is_alive():
-            logger.info(f"Shutting down process '{p.name}'...")
-            p.kill()
-            p.join()
-        logger.info("All processes shut down.")
-        sys.exit(exit_code)
